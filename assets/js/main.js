b53b98bf79e93415d2ea09bc5b671d91aabd5d0c
Maria Mir <maria-mir@hotmail.com>
2019-02-16 18:00:39 +0000

    Random computer sequence generated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,17 @@
 $(document).ready(function(){
    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+// function to generate random color sequence for computer
+   var colors = ["red", "green", "blue", "yellow"];
+    var computerSequence = [];
+   function generateRandomColorSequence(sequenceLength) {
+       for (var i=0; i<sequenceLength; i++) {
+            var randomNumber = Math.floor(Math.random()*4);
+           computerSequence.push(colors[randomNumber]);
+       }
+       return computerSequence;
+   }
+    // function test
+    console.log(generateRandomColorSequence(7));
     
     
 });
