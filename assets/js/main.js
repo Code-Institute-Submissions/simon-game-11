5e84280999dbdae3f458979f70a39eeea311b7bb
Maria Mir <maria-mir@hotmail.com>
2019-02-16 18:47:41 +0000

    Sound and other effects added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+
+// Global variables    
+    var red = $('#red');
+    var green = $('#green');
+    var blue = $('#blue');
+    var yellow = $('#yellow');
+    var redSound = $('#redSound')[0];
+    var greenSound = $('#greenSound')[0];
+    var blueSound = $('#blueSound')[0];
+    var yellowSound = $('#yellowSound')[0];
    
-// function to generate random color sequence for computer
+// Function to generate random color sequence for computer
    var colors = ["red", "green", "blue", "yellow"];
     var computerSequence = [];
    function generateRandomColorSequence(sequenceLength) {
@@ -10,8 +20,31 @@ $(document).ready(function(){
        }
        return computerSequence;
    }
-    // function test
-    console.log(generateRandomColorSequence(7));
-    
+   
+// Handling events
+    red.on('click', function (){ 
+        play_sound(red, redSound);
+    });
+    green.on('click', function (){
+        play_sound(green, greenSound);
+    });
+    yellow.on('click', function (){
+        play_sound(yellow, yellowSound);
+    });
+    blue.on('click', function (){
+        play_sound(blue, blueSound);
+    });
+
+// function to play sound and other effects
+    function play_sound(colorId, soundId) {
+        soundId.play();
+        // shadow effect added
+        colorId.addClass("shadow-effect");
+        // shadow effect removed
+        setTimeout(no_shadow_effect, 100);
+        function no_shadow_effect(){
+            colorId.removeClass("shadow-effect");
+        }
+    }
     
 });
